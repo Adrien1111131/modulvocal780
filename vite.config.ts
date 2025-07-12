a83bb06dd2434bd0b886ad9082bd0eccacab3105
Adrien <adrien@example.com>
2025-07-12 21:24:54 +0200

    Configuration GitHub Pages - Ajout homepage et base path pour déploiement

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return {
-    base: '/',
+    base: '/modulvocal7/',
     plugins: [react()],
     server: {
       port: 3002,
